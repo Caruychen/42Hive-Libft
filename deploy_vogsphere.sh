f0e9c01af7de96fc128e88a06aae7cec363ad725
Caruy Chen <cchen@hive.fi>
2021-11-01 17:03:28 +0200

    fix repo name issues

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,14 +6,14 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/11/01 16:55:06 by cchen            ###   ########.fr        #
+#    Updated: 2021/11/01 17:03:01 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # To be executed in github actions environment. Takes all necessary .c and .h
 # files, and places them in the root repositor for submission
 
-cp ./${{ github.event.repository.name }}/author ./libft_vogsphere
-cp ./${{ github.event.repository.name }}/Makefile ./libft_vogsphere
-cp ./${{ github.event.repository.name }}/srcs/* ./libft_vogsphere
-cp ./${{ github.event.repository.name }}/includes/* ./libft_vogsphere
+cp ./${{ REPO_NAME }}/author ./libft_vogsphere
+cp ./${{ REPO_NAME }}/Makefile ./libft_vogsphere
+cp ./${{ REPO_NAME }}/srcs/* ./libft_vogsphere
+cp ./${{ REPO_NAME }}/includes/* ./libft_vogsphere
