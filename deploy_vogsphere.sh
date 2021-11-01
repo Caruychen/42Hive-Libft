288fd0d3f5a8269d898e89ba9a22ed5209f9a5f4
Caruy Chen <cchen@hive.fi>
2021-11-01 17:07:26 +0200

    fix environment variable issue for actions

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,14 +6,14 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/11/01 17:03:01 by cchen            ###   ########.fr        #
+#    Updated: 2021/11/01 17:07:01 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # To be executed in github actions environment. Takes all necessary .c and .h
 # files, and places them in the root repositor for submission
 
-cp ./${{ REPO_NAME }}/author ./libft_vogsphere
-cp ./${{ REPO_NAME }}/Makefile ./libft_vogsphere
-cp ./${{ REPO_NAME }}/srcs/* ./libft_vogsphere
-cp ./${{ REPO_NAME }}/includes/* ./libft_vogsphere
+cp ./$REPO_NAME/author ./libft_vogsphere
+cp ./$REPO_NAME/Makefile ./libft_vogsphere
+cp ./$REPO_NAME/srcs/* ./libft_vogsphere
+cp ./$REPO_NAME/includes/* ./libft_vogsphere
