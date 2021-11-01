cdea2683e22f04d4304ec4d658ecc7ede7799f1f
Caruy Chen <cchen@hive.fi>
2021-11-01 16:56:07 +0200

    use repository name via variable

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,14 +6,14 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/11/01 14:45:27 by cchen            ###   ########.fr        #
+#    Updated: 2021/11/01 16:55:06 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # To be executed in github actions environment. Takes all necessary .c and .h
 # files, and places them in the root repositor for submission
 
-cp ./42Hive-Libft/author ./libft_vogsphere
-cp ./42Hive-Libft/Makefile ./libft_vogsphere
-cp ./42Hive-Libft/srcs/* ./libft_vogsphere
-cp ./42Hive-Libft/includes/* ./libft_vogsphere
+cp ./${{ github.event.repository.name }}/author ./libft_vogsphere
+cp ./${{ github.event.repository.name }}/Makefile ./libft_vogsphere
+cp ./${{ github.event.repository.name }}/srcs/* ./libft_vogsphere
+cp ./${{ github.event.repository.name }}/includes/* ./libft_vogsphere
