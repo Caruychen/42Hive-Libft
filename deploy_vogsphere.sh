f7039c0067d003b80f14b6343b5c3b12a3160960
Caruy Chen <cchen@hive.fi>
2021-11-02 11:56:01 +0200

    fix target repo in deploy_vogsphere.sh

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,14 +6,14 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/11/02 11:34:09 by cchen            ###   ########.fr        #
+#    Updated: 2021/11/02 11:55:41 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # To be executed in github actions environment. Takes all necessary .c and .h
 # files, and places them in the root repositor for submission
 
-cp ./$REPO_NAME/author ./libft_vogsphere
-cp ./$REPO_NAME/Makefile ./libft_vogsphere
-cp ./$REPO_NAME/srcs/* ./libft_vogsphere
-cp ./$REPO_NAME/includes/* ./libft_vogsphere
+cp ./$REPO_NAME/author ./intra-uuid-*
+cp ./$REPO_NAME/Makefile ./intra-uuid-*
+cp ./$REPO_NAME/srcs/* ./intra-uuid-*
+cp ./$REPO_NAME/includes/* ./intra-uuid-*
