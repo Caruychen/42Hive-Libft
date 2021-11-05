6a52a6d5d83ace41e50ea2966937f1ccb22ff915
Caruychen <caruychen@gmail.com>
2021-11-05 09:58:00 +0200

    added ft_memcmp

---
+++ srcs/ft_memcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 09:40:13 by cchen             #+#    #+#             */
+/*   Updated: 2021/11/05 09:53:30 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+
+	if (n == 0)
+		return (0);
+	ptr1 = s1;
+	ptr2 = s2;
+	while (n--)
+	{
+		if (*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+		++ptr1;
+		++ptr2;
+	}
+	return (0);
+}
