2a5c0843839a0fbea5d1a2f6572446ba613cb26b
Caruy Chen <cchen@hive.fi>
2021-11-05 13:55:51 +0200

    fix norm

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 20:20:36 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/04 21:37:20 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/05 13:55:39 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char *ptr;
+	unsigned char	*ptr;
 
 	if (n == 0 || s == NULL)
 		return (NULL);
