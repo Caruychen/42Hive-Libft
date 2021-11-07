c672b00b84486ec9c58ae0de7ac700054aeb41b3
Caruychen <caruychen@gmail.com>
2021-11-07 15:40:35 +0200

    Added ft_putchar

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 13:39:10 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/07 15:29:08 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/07 15:39:03 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
+void	ft_putchar(char c);
 
 #endif
