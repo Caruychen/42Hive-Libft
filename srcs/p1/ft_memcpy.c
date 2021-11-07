96375176cbcad96cabb563abdace89a2c96d65e3
Caruychen <caruychen@gmail.com>
2021-11-07 21:56:50 +0200

    update memcpy

--- srcs/p1/ft_memcpy.c
+++ srcs/p1/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 08:54:23 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/04 10:47:16 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/07 21:50:46 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,28 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char		*d;
-	const char	*s;
+	unsigned long		*d_l;
+	const unsigned long	*s_l;
+	unsigned char		*d_c;
+	const unsigned char	*s_c;
+	unsigned int		chunks;
+	unsigned int		trail;
 
-	d = (char *)dst;
-	s = (const char *)src;
 	if (n == 0 || dst == src)
 		return (dst);
-	while (n--)
-		*d++ = *s++;
+	chunks = n / sizeof(dst);
+	trail = n % sizeof(dst);
+	d_l = (unsigned long *)dst;
+	s_l = (const unsigned long *)src;
+	d_c = (unsigned char *)dst;
+	s_c = (const unsigned char *)src;
+	while (chunks--)
+	{
+		*d_l++ = *s_l++;
+		d_c += sizeof(d_l);
+		s_c += sizeof(s_l);
+	}
+	while (trail--)
+		*d_c++ = *s_c++;
 	return (dst);
 }
