98fe1f2817f9e981015762bb3d0d16c286dffbbc
Caruy Chen <cchen@hive.fi>
2021-11-08 10:59:39 +0200

    udpated functions

--- srcs/p1/ft_memcpy.c
+++ srcs/p1/ft_memcpy.c
@@ -6,13 +6,14 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 08:54:23 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/08 09:29:12 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/08 10:45:35 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_copy_long(unsigned char **dst, const unsigned char **src, size_t n)
+static void	ft_copy_chunks(unsigned char **dst,
+		const unsigned char **src, size_t n)
 {
 	unsigned long		*d;
 	const unsigned long	*s;
@@ -25,41 +26,37 @@ static void	ft_copy_long(unsigned char **dst, const unsigned char **src, size_t 
 		*d++ = *s++;
 }
 
-static void	ft_copy_trail(unsigned char **dst, const unsigned char **src, size_t n)
+static void	ft_copy_trail(unsigned char **dst,
+		const unsigned char **src, size_t n)
 {
-	unsigned int	trail_4bytes;
-	unsigned int	trail_2bytes;
+	unsigned int	chunk;
 	unsigned int	trail;
 
-	trail_4bytes = n / sizeof(int);
-	trail_2bytes = n % sizeof(int);
-	trail = n % sizeof(short);
-	while (trail_4bytes--)
-		*(*(unsigned int**)dst)++ = *(*(const unsigned int **)src)++;
-	while (trail_2bytes--)
-		*(*(unsigned short**)dst)++ = *(*(const unsigned short **)src)++;
-	while (trail--)
+	chunk = n / sizeof(int);
+	trail = n % sizeof(int); 
+	if (chunk)
+		*(*(unsigned int **)dst)++ = *(*(const unsigned int **)src)++;
+	if (trail / sizeof(short))
+		*(*(unsigned short **)dst)++ = *(*(const unsigned short **)src)++;
+	if  (n % sizeof(short))
 		*(*dst)++ = *(*src)++;
 }
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char		*d_c;
-	const unsigned char	*s_c;
+	unsigned char		*d;
+	const unsigned char	*s;
 	unsigned int		chunks;
 	unsigned int		trail;
 
 	if (n == 0 || dst == src)
 		return (dst);
-	d_c = (unsigned char *)dst;
-	s_c = (const unsigned char *)src;
+	d = (unsigned char *)dst;
+	s = (const unsigned char *)src;
 	chunks = n / sizeof(long);
 	trail = n % sizeof(long);
 	if (chunks)
-		ft_copy_long(&d_c, &s_c, chunks);
-	/*while (trail--)
-		*d_c++ = *s_c++;
-	*/
-	ft_copy_trail(&d_c, &s_c, trail);
+		ft_copy_chunks(&d, &s, chunks);
+	ft_copy_trail(&d, &s, trail);
 	return (dst);
 }
