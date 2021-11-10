e44685dd4d1ec381397fc27407907f42a2c5b5b0
Caruychen <caruychen@gmail.com>
2021-11-10 15:08:12 +0200

    added putendl_fd

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 13:39:10 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/10 10:55:45 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/10 12:53:22 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ void	ft_putstr(char const *s);
 void	ft_putendl(char const *s);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl_fd(char const *s, int fd);
 
 #endif
