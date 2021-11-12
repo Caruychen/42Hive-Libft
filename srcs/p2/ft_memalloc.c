8a8aefa58071c425db2d061d37d30569cdc028bb
Caruy Chen <caruychen@gmail.com>
2021-11-12 13:14:42 +0200

    fix norm

--- srcs/p2/ft_memalloc.c
+++ srcs/p2/ft_memalloc.c
@@ -6,11 +6,12 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 11:26:23 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/12 13:00:23 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/12 13:05:12 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 void	*ft_memalloc(size_t size)
 {
 	char	*mem;
