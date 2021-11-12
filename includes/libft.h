f5c366e7e2518d8c9003f38095cd2852ea32ec21
Caruy Chen <caruychen@gmail.com>
2021-11-12 13:25:15 +0200

    added ft_memdel

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 13:39:10 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/12 11:34:50 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/12 13:24:56 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 void	ft_putendl(char const *s);
