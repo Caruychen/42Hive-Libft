09283ed06866566f18a2a8f81d8c6727e0971271
Caruy Chen <caruychen@gmail.com>
2021-11-16 12:24:12 +0200

    added ft_strchr

---
+++ srcs/p1/ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 12:01:06 by cchen             #+#    #+#             */
+/*   Updated: 2021/11/16 12:23:55 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+	char	ch;
+
+	ch = (char)c;
+	ptr = (char *)s;
+	while (*ptr)
+	{
+		if (*ptr == ch)
+			return (ptr);
+		++ptr;
+	}
+	if (*ptr == 0)
+		return (ptr);
+	return (NULL);
+}
