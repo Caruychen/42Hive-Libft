7163173df37a67f2e6e8773c0ab777399b7b41bb
Caruychen <caruychen@gmail.com>
2021-11-17 22:52:43 +0200

    refactor ft_strstr

--- srcs/p1/ft_strstr.c
+++ srcs/p1/ft_strstr.c
@@ -6,37 +6,37 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 13:04:06 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/17 22:15:44 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/17 22:48:50 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_is_match(const char *hay_piece, const char *needle)
+{
+	while (1)
+	{
+		if (*needle == 0)
+			return (1);
+		if (*hay_piece++ != *needle++)
+			return (0);
+	}
+}
+
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	char	*substring;
-	char	*haystack_p;
-
 	if (*needle == 0)
 		return ((char *)haystack);
-	substring = (char *)needle;
 	while (*haystack)
 	{
-		if (*haystack != *substring)
+		if (*haystack != *needle)
 		{
 			++haystack;
 			continue ;
 		}
-		haystack_p = (char *)haystack;
-		while (1)
-		{
-			if (*substring == 0)
-				return ((char *)haystack);
-			if (*haystack_p++ != *substring++)
-				break ;
-		}
+		if (ft_is_match(haystack, needle))
+			return ((char *)haystack);
 		++haystack;
-		substring = (char *)needle;
 	}
 	return (NULL);
 }	
