2068f864fdd885787cb7f4fadabed18513ea926d
Caruy Chen <caruychen@gmail.com>
2021-11-18 15:30:40 +0200

    refactor test_memset

--- tests/units/p1/test_memset.c
+++ tests/units/p1/test_memset.c
@@ -6,16 +6,17 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 10:40:42 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/05 10:43:31 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/18 15:30:28 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "testft.h"
 
-static void	initialize_string(char **str, int length)
+static char	*initialize_string(size_t length)
 {
-	*str = NULL;
-	*str = (char *)malloc(sizeof(**str) * length);
+	char *str;
+	str = (char *)malloc(sizeof(*str) * length);
+	return (str);
 }
 
 static void	fill_strings(char *s1, char *s2, int c, int length)
@@ -24,37 +25,18 @@ static void	fill_strings(char *s1, char *s2, int c, int length)
 	ft_memset(s2, c, length);
 }
 
-static int	compare_strings(char *s1, char *s2, int length)
-{
-	int	index;
-
-	index = 0;
-	while (index < length)
-	{
-		if (s1[index] != s2[index])
-		{
-			printf("FAILED: Error in ft_memset.\n");
-			printf("memset: %s ft_memset: %s\n", s1, s2);
-			return (-1);
-		}
-		++index;
-	}
-	return (0);
-}
-
 static int	cycle_chars(char *s1, char *s2, int index, int length)
 {
-	int	currentInt;
-	int	maxInt;
+	size_t	currentInt = 0;
+	size_t	maxInt = 512;
 
-	currentInt = 0;
-	maxInt = 512;
 	while (currentInt <= maxInt)
 	{
-		fill_strings(s1, s2, currentInt, length - index);
-		if (compare_strings(s1, s2, length) == -1)
+		fill_strings(s1, s2, currentInt, index);
+		if (memcmp(s1, s2, length) == -1)
 		{
-			printf("Tested integer: %d\n", currentInt);
+			printf("FAILED: Error in ft_memset.\n");
+			printf("Expected: %s, got: %s\n", s1, s2);
 			return (-1);
 		}
 		++currentInt;
@@ -62,27 +44,32 @@ static int	cycle_chars(char *s1, char *s2, int index, int length)
 	return (0);
 }
 
-int	test_memset(void)
+int	cycle_memset_tests(void)
 {
-	char	*controlStr;
-	char	*testStr;
-	int		length;
-	int		index;
+	size_t	length = 42;
+	char	*controlStr = initialize_string(length);
+	char	*testStr = initialize_string(length);
+	size_t	index = 0;
+	int		outcome = 0;
 
-	length = 42;
-	initialize_string(&controlStr, length);
-	initialize_string(&testStr, length);
-	if (controlStr == NULL || testStr == NULL)
-		return (-1);
-	index = 0;
 	while (index <= length)
 	{
-		fill_strings(controlStr, testStr, 0, length);
+		bzero(testStr, length);
+		bzero(controlStr, length);
 		if (cycle_chars(controlStr, testStr, index, length) == -1)
-			return (-1);
+			outcome = -1;
 		++index;
 	}
 	free(controlStr);
 	free(testStr);
-	return (0);
+	return (outcome);
+}
+
+int	test_memset(void)
+{
+	int	outcome = 0;
+
+	outcome = cycle_memset_tests() || outcome;
+	print_outcome(outcome, __func__);
+	return (-outcome);
 }
