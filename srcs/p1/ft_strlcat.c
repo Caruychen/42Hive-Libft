873931002fe11b2b01d407d61f2097b7282c8989
Caruy Chen <caruychen@gmail.com>
2021-11-18 15:51:45 +0200

    refactor strlcat

--- srcs/p1/ft_strlcat.c
+++ srcs/p1/ft_strlcat.c
@@ -6,34 +6,41 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 14:19:52 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/18 14:35:44 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/18 15:51:26 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	traverse_dst(char **d, size_t dstsize)
+{
+	char	*start;
+
+	start = *d;
+	while (dstsize-- && **d)
+		++(*d);
+	return (*d - start);
+}
+
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	gap_n;
 	char	*d;
 	char	*s;
+	size_t	bufferlen;
 	size_t	dlen;
 
 	d = (char *)dst;
 	s = (char *)src;
-	gap_n = dstsize;
-	while (gap_n-- && *d)
-		++d;
-	dlen = d - dst;
-	gap_n = dstsize - dlen;
-	if (gap_n == 0)
+	dlen = traverse_dst(&d, dstsize);
+	bufferlen = dstsize - dlen;
+	if (bufferlen == 0)
 		return (dlen + ft_strlen(src));
 	while (*s)
 	{
-		if (gap_n != 1)
+		if (bufferlen != 1)
 		{
 			*d++ = *s;
-			--gap_n;
+			--bufferlen;
 		}
 		++s;
 	}
