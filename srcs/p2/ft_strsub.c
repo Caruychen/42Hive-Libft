5a06e40015b4e7f9783d25713ab61de1e850d96e
Caruy Chen <caruychen@gmail.com>
2021-11-18 15:52:00 +0200

    Added preliminary  ft_strsub

---
+++ srcs/p2/ft_strsub.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 15:34:16 by cchen             #+#    #+#             */
+/*   Updated: 2021/11/18 15:35:49 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+
+	sub = (char *)malloc(sizeof(*sub) * len);
+	return (sub);
+}
