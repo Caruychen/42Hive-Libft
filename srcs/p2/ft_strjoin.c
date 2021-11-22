d31a28779589f5eeb8f89e7fc4fe70b22595f8b5
Caruy Chen <caruychen@gmail.com>
2021-11-22 15:31:38 +0200

    added malloc return protection

--- srcs/p2/ft_strjoin.c
+++ srcs/p2/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 20:52:18 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/18 21:24:06 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/22 15:31:02 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	str = (char *)malloc(sizeof(*str) * (len1 + len2 + 1));
+	if (!str)
+		return (NULL);
 	ft_memcpy(str, s1, len1);
 	ft_memcpy(str + len1, s2, len2);
 	str[len1 + len2] = 0;
