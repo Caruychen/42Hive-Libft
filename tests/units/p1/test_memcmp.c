d81a6223907af6d6549701ac7f38653b0a3ea8ef
Caruy Chen <caruychen@gmail.com>
2021-11-22 11:30:50 +0200

    working on tests

--- tests/units/p1/test_memcmp.c
+++ tests/units/p1/test_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 09:59:38 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/16 10:17:31 by cchen            ###   ########.fr       */
+/*   Updated: 2021/11/22 11:22:30 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	run_comparisons(char *s1, char *s2)
 	size_t	n;
 	size_t	index;
 
-	n = find_max_len(s1, s2);
+	n = 7;
 	index = 0;
 	while (index < n + 1)
 	{
@@ -36,14 +36,14 @@ int	test_memcmp(void)
 {
 	int	outcome;
 	char	*arr1[] = {
-		"foo bar", "foo\0bar", "foo", "bar",
+		"foo bar", "foo\0bar", "foo\0foo", "foo", "bar",
 		"", "\0", "\200", "a", "A",
 		"1"};
 	char	*arr2[] = {
-		"foo bar", "foo\0bar", "foo", "bar",
+		"foo bar", "foo\0bar", "foo\0foo", "foo", "bar",
 		"", "\0", "\200", "a", "A",
 		"1"};
-	int	length = 10;
+	int	length = 11;
 
 	outcome = 0;
 	outcome = iterate_dual_arrays(arr1, arr2, length, run_comparisons) || outcome;
