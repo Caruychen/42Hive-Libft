a2a32f307ec0f17c1e8271240aaea1c641fdfd84
Caruychen <caruychen@gmail.com>
2021-11-24 11:40:07 +0200

    update script to replace destination

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,16 +6,16 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/11/22 17:23:05 by cchen            ###   ########.fr        #
+#    Updated: 2021/11/24 11:39:44 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # To be executed in github actions environment. Takes all necessary .c and .h
 # files, and places them in the root repositor for submission
 
-cp ./$REPO_NAME/author ./intra-uuid-*
-cp ./$REPO_NAME/Makefile ./intra-uuid-*
-cp ./$REPO_NAME/srcs/p1/* ./intra-uuid-*
-cp ./$REPO_NAME/srcs/p2/* ./intra-uuid-*
-cp ./$REPO_NAME/srcs/bonus/* ./intra-uuid-*
-cp ./$REPO_NAME/includes/* ./intra-uuid-*
+cp -f ./$REPO_NAME/author ./intra-uuid-*
+cp -f ./$REPO_NAME/Makefile ./intra-uuid-*
+cp -f ./$REPO_NAME/srcs/p1/* ./intra-uuid-*
+cp -f ./$REPO_NAME/srcs/p2/* ./intra-uuid-*
+cp -f ./$REPO_NAME/srcs/bonus/* ./intra-uuid-*
+cp -f ./$REPO_NAME/includes/* ./intra-uuid-*
