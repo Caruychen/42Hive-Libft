b819ff0decda3b325b6ad2e1332269f71a7880ee
Caruychen <caruychen@gmail.com>
2021-12-06 21:30:27 +0200

    refactor strnequ

--- srcs/p2/ft_strnequ.c
+++ srcs/p2/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 11:04:01 by cchen             #+#    #+#             */
-/*   Updated: 2021/11/16 11:07:37 by cchen            ###   ########.fr       */
+/*   Updated: 2021/12/06 21:21:24 by cchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,5 @@
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	int	res;
-
-	res = ft_strncmp(s1, s2, n);
-	if (res)
-		return (0);
-	return (1);
+	return (ft_strncmp(s1, s2, n) == 0);
 }
