8faeca9d3cf0003273b4b65dd2cd8a84f8c32ec7
Caruy Chen <caruychen@gmail.com>
2021-12-07 12:50:12 +0200

    Test

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,7 +6,7 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/12/07 12:49:04 by cchen            ###   ########.fr        #
+#    Updated: 2021/12/07 12:50:06 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@
 rm -rf ./intra-uuid-*/*
 sed '/prod/,$d' ./$REPO_NAME/Makefile > ./$REPO_NAME/tempMake
 cp -f ./$REPO_NAME/tempMake ./intra-uuid-*
-mv ./intra-uuid-*/tempMake ./intra-uuid-\*/Makefile
+mv ./intra-uuid-*/tempMake ./intra-uuid-*\Makefile
 cp ./$REPO_NAME/author ./intra-uuid-*
 cp ./$REPO_NAME/Makefile ./intra-uuid-*
 cp ./$REPO_NAME/srcs/p1/* ./intra-uuid-*
