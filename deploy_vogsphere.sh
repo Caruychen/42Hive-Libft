ba93cff0fb6a7ace96c04b902b00b630f34389b8
Caruy Chen <caruychen@gmail.com>
2021-12-07 12:19:32 +0200

    set up vogsphere repo in env

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,17 +6,17 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/12/07 12:10:15 by cchen            ###   ########.fr        #
+#    Updated: 2021/12/07 12:18:22 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # To be executed in github actions environment. Takes all necessary .c and .h
 # files, and places them in the root repositor for submission
 
-rm -rf ./intra-uuid-*/*
-cp ./$REPO_NAME/author ./intra-uuid-*
-sed '/prod/,$d' ./$REPO_NAME/Makefile > ./intra-uuid-*/Makefile
-cp ./$REPO_NAME/srcs/p1/* ./intra-uuid-*
-cp ./$REPO_NAME/srcs/p2/* ./intra-uuid-*
-cp ./$REPO_NAME/srcs/bonus/* ./intra-uuid-*
-cp ./$REPO_NAME/includes/* ./intra-uuid-*
+rm -rf ./$VOGSPHERE/*
+cp ./$REPO_NAME/author ./$VOGSPHERE
+sed '/prod/,$d' ./$REPO_NAME/Makefile > ./$VOGSPHERE/Makefile
+cp ./$REPO_NAME/srcs/p1/* ./$VOGSPHERE
+cp ./$REPO_NAME/srcs/p2/* ./$VOGSPHERE
+cp ./$REPO_NAME/srcs/bonus/* ./$VOGSPHERE
+cp ./$REPO_NAME/includes/* ./$VOGSPHERE
