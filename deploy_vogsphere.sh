78adbc13f381c3018fefe878623a1f0c6282c1b1
Caruy Chen <caruychen@gmail.com>
2021-12-07 14:27:53 +0200

    Test

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,7 +6,7 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/12/07 14:25:49 by cchen            ###   ########.fr        #
+#    Updated: 2021/12/07 14:27:50 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,9 +14,8 @@
 # files, and places them in the root repositor for submission
 
 rm -rf ./intra-uuid-*/*
-sed '/prod/,$d' ./$REPO_NAME/Makefile > ./intra-uuid-**/Makefile
+sed '/prod/,$d' ./$REPO_NAME/Makefile > ./intra-uuid-.*/Makefile
 cp ./$REPO_NAME/author ./intra-uuid-*
-#cp ./$REPO_NAME/Makefile ./intra-uuid-*
 cp ./$REPO_NAME/srcs/p1/* ./intra-uuid-*
 cp ./$REPO_NAME/srcs/p2/* ./intra-uuid-*
 cp ./$REPO_NAME/srcs/bonus/* ./intra-uuid-*
