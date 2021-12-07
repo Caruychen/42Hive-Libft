8ccacfd281700d31f496b94522da38608a9e2b29
Caruy Chen <caruychen@gmail.com>
2021-12-07 12:10:27 +0200

    fix deploy logic

--- deploy_vogsphere.sh
+++ deploy_vogsphere.sh
@@ -6,7 +6,7 @@
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/01 13:56:45 by cchen             #+#    #+#              #
-#    Updated: 2021/12/07 12:06:22 by cchen            ###   ########.fr        #
+#    Updated: 2021/12/07 12:10:15 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,8 +15,7 @@
 
 rm -rf ./intra-uuid-*/*
 cp ./$REPO_NAME/author ./intra-uuid-*
-touch ./intra-uuid-*/Makefile
-sed '/\nprod/,$d' ./$REPO_NAME/Makefile > ./intra-uuid-*/Makefile
+sed '/prod/,$d' ./$REPO_NAME/Makefile > ./intra-uuid-*/Makefile
 cp ./$REPO_NAME/srcs/p1/* ./intra-uuid-*
 cp ./$REPO_NAME/srcs/p2/* ./intra-uuid-*
 cp ./$REPO_NAME/srcs/bonus/* ./intra-uuid-*
