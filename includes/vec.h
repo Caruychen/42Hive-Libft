26aec2120c3b7f348d49fda79f9b223e1552f55e
Caruychen <caruychen@gmail.com>
2021-12-20 11:06:08 +0200

    initialise vector header

---
+++ includes/vec.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 11:01:19 by cchen             #+#    #+#             */
+/*   Updated: 2021/12/20 11:05:48 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VEC_H
+# define VEC_H
+
+# include <string.h>
+
+typedef struct s_vec
+{
+	void			*memory;
+	size_t			elem_size;
+	size_t			alloc_size;
+	size_t			len;
+}		t_vec;
+
+int	vec_new(t_vec *src, size_t len, size_t elem_size);
+
+#endif
